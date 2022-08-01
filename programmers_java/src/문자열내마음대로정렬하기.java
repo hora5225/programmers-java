4da536c977b2b0ec757fed1561220d6ffd1f094f
hora5225 <hora5225@gmail.com>
2022-08-01 23:59:13 +0900

    시작

---
+++ programmers_java/src/문자열내마음대로정렬하기.java
@@ -0,0 +1,89 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+//class Solution {
+//    public String[] solution(String[] strings, int n) {
+//        Arrays.sort(strings, new Comparator<String>() {
+//
+//            @Override
+//            public int compare(String s1, String s2){
+//                if(s1.charAt(n) > s2.charAt(n)) return 1;
+//                else if(s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
+//                else if(s1.charAt(n) < s2.charAt(n)) return -1;
+//                else return 0;
+//            }
+//
+//        });
+//        return strings;
+//        }
+//    }
+
+
+class 문자열내마음대로정렬하기 {
+    public String[] solution(String[] strings, int n){
+        String[] answer = new String[strings.length];
+
+        ArrayList<String> stringArr = new ArrayList<>();
+
+        for(String string : strings) {
+            stringArr.add(string.charAt(n) + string);
+        }
+        //[usun, ebed, acar] 이 됨
+
+        Collections.sort(stringArr);
+        //[acar, ebed, usun]이 됨
+
+        for (int i = 0; i < stringArr.size(); i++) {
+            //i번째 리스트를 get 한 다음 인덱스 1부터 끝까지를 answer[i]에 넣어줌
+            answer[i] = stringArr.get(i).substring(1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        문자열내마음대로정렬하기 s = new 문자열내마음대로정렬하기();
+        String[] strings = {"sun", "bed", "car"};
+        int n = 1;
+        System.out.println(Arrays.toString(s.solution(strings, n)));
+    }
+}
+
+
+/*
+class 문자열내마음대로정렬하기 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] strings = {"sun", "bed", "car"};
+        int n = 1;
+        System.out.println(Arrays.toString(s.solution(strings, n)));
+    }
+}
+
+class Solution {
+    public String[] solution(String[] strings, int n){
+        String[] answer = new String[strings.length];
+
+        ArrayList<String> stringArr = new ArrayList<>();
+
+        for(String string : strings) {
+            stringArr.add(string.charAt(n) + string);
+        }
+        //[usun, ebed, acar] 이 됨
+
+        Collections.sort(stringArr);
+        //[acar, ebed, usun]이 됨
+
+        for (int i = 0; i < stringArr.size(); i++) {
+            //i번째 리스트를 get 한 다음 인덱스 1부터 끝까지를 answer[i]에 넣어줌
+            answer[i] = stringArr.get(i).substring(1);
+        }
+        return answer;
+    }
+}
+*/
+
+
+
---
+++ programmers_java/src/체육복.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java
+ * 220801
+ */
+public class 체육복 {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        int[] students = new int[n];
+
+        //도둑맞은 사람은 -1
+        for (int l : lost)
+            students[l-1]--;
+
+        //reserve인 사람은 +1
+        for (int r : reserve)
+            students[r-1]++;
+
+        for (int i = 0; i < students.length; i++) {
+            if(students[i] == -1) { //일단 -1인 잃어버린 사람을 찾는다
+                //양쪽에 있는 사람을 찾아서 넘겨주고, 줬으면 -1 해줘서 0으로 만들어줘서 또 못주게 한다
+                if(i-1>=0 && students[i-1] == 1) {
+                    students[i]++;
+                    students[i-1]--;
+                }else if(i+1< students.length && students[i+1] == 1) {
+                    students[i]++;
+                    students[i+1]--;
+                }else   //받을사람이 없을 때 (수업못함)
+                    answer--;
+            }
+        }
+        return answer;
+    }
+}
