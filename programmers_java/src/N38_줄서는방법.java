5d8f44b5f541faa4bd92bb7d65af58bb3dabc7b3
hora5225 <hora5225@gmail.com>
2023-01-11 11:15:22 +0900

    230111

---
+++ programmers_java/src/N38_줄서는방법.java
@@ -0,0 +1,20 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12936
+ * 220820*
+ */
+
+public class N38_줄서는방법 {
+    public int[] solution(int n, long k) {
+        int[] answer = {};
+        return answer;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
